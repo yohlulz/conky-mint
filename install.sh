2a99eb7b9fad239bb9aab9be2e3d6061e1fb28b3
Ovidiu Maja <ovi.dan89@gmail.com>
2017-05-10 00:49:24 +0300

    Fixed startup script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ${SCRIPT_DIR}/weather.sh
 chmod +x ${SCRIPT_DIR}/conkystart.sh
 
 ln -sf ${SCRIPT_DIR}/${resource}/conkyrc ~/.conkyrc
-ln -sf ${SCRIPT_DIR}/${resource}/conkystart.sh ~/.conkystart.sh
+ln -sf ${SCRIPT_DIR}/conkystart.sh ~/.conkystart.sh
 
 sed -i "s|__LOGO__|${SCRIPT_DIR}/${resource}/logo.png|g" ${SCRIPT_DIR}/${resource}/conkyrc
 sed -i "s|__CLOCK_RINGS__|${SCRIPT_DIR}/${resource}/clock_rings.lua|g" ${SCRIPT_DIR}/${resource}/conkyrc
